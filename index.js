369ea04d90f03ceba251c2868b52c52a7abcdfa2
anti-duhring <mateusvnlima@gmail.com>
2022-11-01 20:04:59 -0300

    feat: add configurações do mongoose

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const port = 3000;
+const DBuser = 'admin';
+const DBpassword = 'admin';
 
+// middleware para ler JSON
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+)
+app.use(express.json())
+
+// Rotas 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.json({message: 'Hello world'})
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+// Listen
+mongoose.connect(`mongodb+srv://${DBuser}:${DBpassword}@cluster0.ml7wdi3.mongodb.net/database?retryWrites=true&w=majority`)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server running on http://localhost:${port}`);
+    });
+  })
+  .catch((e) => console.log(e))
