5aaf4180d9129a5a3e3de23e1cbd27a8ca7edc90
MarcosGruah <110102063+MarcosGruah@users.noreply.github.com>
2022-11-03 21:35:23 -0300

    refactor: API routes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const routes = require('./routes')
 const app = express();
+const trilhasRouter = require('./routes/trilhas');
+const notionRouter = require('./routes/notions');
+const healthRouter = require('./routes/health');
 
-app.use(express.json())
-app.use(routes)
-module.exports = app
+app.use(express.json());
+app.use('/trilhas', trilhasRouter);
+app.use('/notions', notionRouter);
+app.use('/health', healthRouter);
+module.exports = app;
