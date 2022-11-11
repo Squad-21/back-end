455660c1ef74999431901e612ddbf2535d142eba
MarcosGruah <110102063+MarcosGruah@users.noreply.github.com>
2022-11-10 22:15:17 -0300

    refactor: pequeno ajuste nos comentarios

--- src/routes/lessons.js
+++ src/routes/lessons.js
@@ -4,13 +4,13 @@ const LessonController = require('../Controllers/LessonController');
 const authMiddleware = require('../middlewares/authenticate');
 const loginMiddleware = require('../middlewares/login');
 
-// Retorna todas as aulas de um curso  - TODO
+// Retornar todas as aulas de um curso  - TODO
 router.get('/:id', LessonController.index);
 
-// Dar 'like' em uma aula - TODO
+// Dar um 'like' em uma aula - TODO
 router.post('/:id/like', loginMiddleware, LessonController.like);
 
-// Dar 'unlike' em uma aula  - TODO
+// Dar um 'unlike' em uma aula  - TODO
 router.post('/:id/unlike', loginMiddleware, LessonController.unlike);
 
 // Deixar um comentário em uma aula - TODO
@@ -25,13 +25,13 @@ router.delete('/:id/comment', authMiddleware, LessonController.deleteComment);
 // Remover comentário de uma lesson - TODO ??? qual a diferenca entre essa e a de cima
 router.post('/:id/done', loginMiddleware, LessonController.markAsDone);
 
-// Adicionar nova lesson - TODO
+// Adicionar uma nova aula - TODO
 router.post('/', authMiddleware, LessonController.store);
 
-// Atualizar uma lesson específica - TODO
+// Atualizar uma aula específica - TODO
 router.put('/:id', authMiddleware, LessonController.update);
 
-// Deletar uma lesson específica - TODO
+// Remover uma aula específica - TODO
 router.delete('/:id', authMiddleware, LessonController.delete);
 
 module.exports = router;
