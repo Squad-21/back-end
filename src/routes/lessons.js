6903594896221aa79b244727cf1e17d3c1655cb2
MarcosGruah <110102063+MarcosGruah@users.noreply.github.com>
2022-11-13 11:06:25 -0300

    refactor: postman API doc. feita.

--- src/routes/lessons.js
+++ src/routes/lessons.js
@@ -4,34 +4,36 @@ const LessonController = require('../Controllers/LessonController');
 const authMiddleware = require('../middlewares/authenticate');
 const loginMiddleware = require('../middlewares/login');
 
-// Retornar todas as aulas de um curso  - TODO
+// Retornar todas as aulas de um curso  - OK
 router.get('/:id', LessonController.index);
 
-// Dar um 'like' em uma aula - TODO
+// Dar um 'like' em uma aula - OK
 router.post('/:id/like', loginMiddleware, LessonController.like);
 
-// Dar um 'unlike' em uma aula  - TODO
+// Dar um 'unlike' em uma aula  - OK
 router.post('/:id/unlike', loginMiddleware, LessonController.unlike);
 
-// Deixar um comentário em uma aula - TODO
+// Deixar um comentário em uma aula - OK
 router.post('/:id/comment', loginMiddleware, LessonController.comment);
 
-// Retorna todos os comentários de uma aula - TODO
+//  Retorna todos os comentários de uma aula - OK
 router.get('/:id/comment', loginMiddleware, LessonController.indexComment);
 
-// Deletar comentário de uma lesson - TODO ??? qual a diferenca entre essa e a de baixo
+// Remover um comentário de uma aula - OK
 router.delete('/:id/comment', authMiddleware, LessonController.deleteComment);
 
-// Remover comentário de uma lesson - TODO ??? qual a diferenca entre essa e a de cima
+// Marcar aula como completa. - OK
 router.post('/:id/done', loginMiddleware, LessonController.markAsDone);
 
-// Adicionar uma nova aula - TODO
+// TODO: Marcar aula que já foi completada como incompleta caso o ususário queira fazer o curso mais uma vez.
+
+// Adicionar uma nova aula - OK
 router.post('/', authMiddleware, LessonController.store);
 
-// Atualizar uma aula específica - TODO
+// Atualizar uma aula específica - OK
 router.put('/:id', authMiddleware, LessonController.update);
 
-// Remover uma aula específica - TODO
+// Remover uma aula específica - OK
 router.delete('/:id', authMiddleware, LessonController.delete);
 
 module.exports = router;
